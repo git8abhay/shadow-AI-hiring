4ed8baaaa369c6760a5456ff5f01511b0afc576b
Abhay Chauhan <167683487+git8abhay@users.noreply.github.com>
2026-07-25 01:18:32 +0530

    Update axios base URL for API requests

--- shadow-frontend/src/lib/axiosconfig.ts
+++ shadow-frontend/src/lib/axiosconfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://shadow-backend-seven.up.railway.app/api",
+  baseURL: "https://shadow-backend-8mqv.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
