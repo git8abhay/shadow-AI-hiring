f8fa9dc5fe454ada98e70e92fcf9a71c3aa87510
Abhay Chauhan <167683487+git8abhay@users.noreply.github.com>
2026-07-25 16:55:35 +0530

    Update axios baseURL to include '/api' path

--- shadow-frontend/src/lib/axiosconfig.ts
+++ shadow-frontend/src/lib/axiosconfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://shadow-backend-8mqv.onrender.com",
+  baseURL: "https://shadow-backend-8mqv.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
