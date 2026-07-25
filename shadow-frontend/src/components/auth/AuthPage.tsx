f5a7d2de04314518cca87e870de8e0cb5fbd836f
Abhay Chauhan <167683487+git8abhay@users.noreply.github.com>
2026-07-25 16:25:35 +0530

    Update AUTH_PATH to use '/api/auth'

--- shadow-frontend/src/components/auth/AuthPage.tsx
+++ shadow-frontend/src/components/auth/AuthPage.tsx
@@ -124,7 +124,7 @@ export default function AuthPage() {
     setIsLoading(true);
 
     try {
-      const AUTH_PATH = "/auth";
+      const AUTH_PATH = "/api/auth";
       let response;
 
       if (authMode === "signup") {
